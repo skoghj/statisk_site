c9d2ec2dad8547058884f510f9e3841988c7f8d8
Huijin <huba0001@stud.kea.dk>
2024-01-31 03:12:08 +0100

    init

---
+++ shop.js
@@ -0,0 +1,5 @@
+//////////////////burgermenu//////////////////
+document.getElementById("burgerIcon").addEventListener("click", function () {
+  var navbar = document.getElementById("myNavbar");
+  navbar.classList.toggle("show");
+});
