3cc4aed64c55340b9f2c546f569dc4f6b6ede5f2
Huijin <huba0001@stud.kea.dk>
2024-02-03 09:53:11 +0100

    hide discount null

--- shop.js
+++ shop.js
@@ -29,15 +29,20 @@ function showProduktListe(productlisteJSON) {
   productlisteJSON.forEach((produktlist) => {
     console.log("produktlist", produktlist);
     produktListeClone = produktelisteTemplate.cloneNode(true).content;
-    produktListeClone.querySelector(".produktliste_image").src = produktlist.image_url;
+    produktListeClone.querySelector(".produktliste_image").src = "https://kea-alt-del.dk/t7/images/jpg/640/1552.jpg "; //jeg kunne ikke finde forskellige billede data
     produktListeClone.querySelector(".produktliste_image").alt = `Picture of a ${produktlist.productdisplayname}`;
     produktListeClone.querySelector(".produkt_name").textContent = produktlist.productdisplayname;
     produktListeClone.querySelector(".articletype").textContent = produktlist.articletype;
     produktListeClone.querySelector(".price").textContent = produktlist.price;
     produktListeClone.querySelector(".discounted").textContent = `-${produktlist.discount}%`;
 
-    if (produktlist.soldout) {
-      produktListeClone.querySelector("article").classList.add("sold_out");
+    //Check if discount is not null before displaying it
+    if (produktlist.discount !== null) {
+      produktListeClone.querySelector(".discounted").textContent = `-${produktlist.discount}%`;
+    } else {
+      // Handle the case when discount is null, e.g., set a default value or hide the element
+      produktListeClone.querySelector(".discounted").textContent = "";
+      produktListeClone.querySelector(".discounted").style.backgroundColor = "transparent";
     }
 
     if (produktlist.sale) {
@@ -49,8 +54,8 @@ function showProduktListe(productlisteJSON) {
       produktListeClone.querySelector(".discounted .prev").textContent = produktlist.price;
     }
 
-    if (produktlist.discounted) {
-      produktListeClone.querySelector("article").classList.add(".discounted");
+    if (produktlist.soldout) {
+      produktListeClone.querySelector("article").classList.add("sold_out");
     }
 
     produktlisteContainer.appendChild(produktListeClone);
